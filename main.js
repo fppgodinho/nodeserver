a69a7c4d6b55919dfe55d7dfddb89e1de07416b0
Fernando Godinho <fppgodinho@gmail.com>
2014-06-13 16:02:26 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var http        = require('http');
+var httpProxy   = require('http-proxy');
+
+var node        = new httpProxy.createProxyServer({
+    target: { host: 'localhost', port: 81 }
+});
+var apache      = new httpProxy.createProxyServer({
+    target: { host: 'localhost', port: 8080 }
+});
+
+var proxyServer = http.createServer(function (req, res)                         {
+    var host    = req.headers.host.split(':')[0];
+    switch(host)                                                                {
+        case 'trcraftingbuddyapi.darkhounds.net':   node.web(req, res);     break;
+        default:                                    apache.web(req, res);   break;
+    }
+    console.log(host);
+}).listen(80);
+
+
+var nodeServer = http.createServer(function (req, res)                          {
+    res.write('Hello world');
+    res.end();
+}).listen(81);
